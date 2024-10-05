b8b21d987770bfa73739380d4ac32ff2f6c65f40
Raushan Kumar <raushan.development@gmail.com>
2024-10-05 18:36:33 +0530

    add: chat ollama model (llama 3.2:3b)

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello World");
+import { ChatOllama } from "@langchain/ollama";
+
+const model = new ChatOllama({
+    model: "llama3.2:latest",
+    baseUrl: "http://localhost:11434",
+    temperature: 0,
+});
+
+// async function main() {
+//     const start = Date.now();
+//     const response = await model.invoke("What is the capital of the moon?");
+//     const end = Date.now();
+//     console.log(response.content);
+//     console.log({ in_token: response.usage_metadata })
+//     console.log(`Time taken: ${end - start}ms`);
+// }
+
+// main();
